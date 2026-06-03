aabf35dd339e79f90f5b67894becb48dd72d5a1e
Guilherme Ganho <guilhermeganho2208@gmail.com>
2026-06-03 16:05:04 -0300

    adicionando métodos para exibir tonalidade e exibir músicas somente com determinada tonalidade

--- Modelos/Musica.cs
+++ Modelos/Musica.cs
@@ -4,15 +4,9 @@ namespace MusicAPI.Consumer.Modelos;
 
 internal class Musica
 {
-    // Em outras linguagens chama-se Anotação. Mas em c#, chama-se Atributo esse código que serve
-    // para indicar qual campo do JSON ela deve mapear [JsonPropertyName("")].
     [JsonPropertyName("song")]
-    // essa linha a cima faz com que essa propriedade "NomeMusica" seja apontada para o
-    // campo song do Json, (geralmente é necessario traduzir APIs para a linguagem determinada.
     public string? NomeMusica { get; set; }
-    //Sem o ?, a propriedade NomeMusica seria obrigada a sempre ter um valor.
-    //Com o ?, ela pode ter um valor ou ser nula.
-    //Na prática, isso é importante porque nem sempre a API vai retornar todos os dados preenchidos.
+
     [JsonPropertyName("artist")]
     public String? Artista { get; set; }
     [JsonPropertyName("genre")]
@@ -20,11 +14,15 @@ internal class Musica
     [JsonPropertyName("duration_ms")]
     public int Duracao { get; set; }
 
+    [JsonPropertyName("key")]
+    public int Key { get; set; }
+
     public void ExibirDetalhesDaMusica()
     {
         Console.WriteLine($"Musica: {NomeMusica}");
         Console.WriteLine($"Artista: {Artista}");
         Console.WriteLine($"Gênero: {Genero}");
         Console.WriteLine($"Duração em segundos: {Duracao /1000}");
+        Console.WriteLine($"Tonalidade: {Key}");
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,22 +3,13 @@ using System.Text.Json;
 using MusicAPI.Consumer.Filtros;
 
 using (HttpClient client = new HttpClient())
-// por padrão a variável HttpClient é chamado de client
 {
-    //try indica o caminho a ser seguido primeiramente, caso tenha erro, entra o catch para ir por outaro caminho, exibindo o erro 
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        //Async é pq não sabemos de fato o tamanho do recurso que estaremos recebendo e ele garante
-        //que vai ser possível receber todos esses recursos, sem o Async,
-        //ele recebe uma parte e já passa para a próxima instrução.
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
-        //Ao adicionar o ! no final, garante que o resultado do Deserialize não vai ser nulo".
+        musicas[0].ExibirDetalhesDaMusica();
         //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas); //1° método.
-        //Um filtro é um mecanismo que seleciona ou organiza dados de acordo com critérios específicos.
-        //criei a classe LinqFilter justamente para isso: aplicar filtros na minha lista
-        //de músicas. O primeiro filtro que desenvolvi foi FiltrarTodosOsGenerosMusicais(),
-        //que pega todos os gêneros únicos da lista e os exibe.
         //LinqOrder.ExibirListaDeArtistasOrdenados(musicas); //2° método.
         //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "rock");// 3° método.
         //LinqFilter.FiltrarMusicasDeUmArtista(musicas, "Michel Teló");// 4° método.
@@ -31,15 +22,15 @@ using (HttpClient client = new HttpClient())
         //musicasFavoritasDoGanho.AdicionarMusicasFavoritas(musicas[5]);
 
 
-        var musicasFavoritasDoGuilherme = new FavoriteSongs("Guilherme");
-        musicasFavoritasDoGuilherme.AdicionarMusicasFavoritas(musicas[532]);
-        musicasFavoritasDoGuilherme.AdicionarMusicasFavoritas(musicas[512]);
-        musicasFavoritasDoGuilherme.AdicionarMusicasFavoritas(musicas[56]);
-        musicasFavoritasDoGuilherme.AdicionarMusicasFavoritas(musicas[21]);
-        musicasFavoritasDoGuilherme.AdicionarMusicasFavoritas(musicas[92]);
+        //var musicasFavoritasDoGuilherme = new FavoriteSongs("Guilherme");
+        //musicasFavoritasDoGuilherme.AdicionarMusicasFavoritas(musicas[532]);
+        //musicasFavoritasDoGuilherme.AdicionarMusicasFavoritas(musicas[512]);
+        //musicasFavoritasDoGuilherme.AdicionarMusicasFavoritas(musicas[56]);
+        //musicasFavoritasDoGuilherme.AdicionarMusicasFavoritas(musicas[21]);
+        //musicasFavoritasDoGuilherme.AdicionarMusicasFavoritas(musicas[92]);
 
-        musicasFavoritasDoGuilherme.ExibirMusicasFavoritas();
-        musicasFavoritasDoGuilherme.GerarArquivoJson();
+        //musicasFavoritasDoGuilherme.ExibirMusicasFavoritas();
+        //musicasFavoritasDoGuilherme.GerarArquivoJson();
     }
     catch (Exception ex)
     {
